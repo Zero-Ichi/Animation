f46a2709eb6c3c11761622077dcfcfa951b15bf3
Ronan <r.dhersignerie@gmail.com>
2018-11-01 17:12:55 +0100

    Initial commit

---
+++ Assets/ClaireController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClaireController : MonoBehaviour {
+
+    [SerializeField]
+    private float walkSpeed = 2;
+	
+    private float axisH;
+    private float axisV;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();    
+    }
+    // Update is called once per frame
+    void Update () {
+        axisH = Input.GetAxis("Horizontal");
+        axisV = Input.GetAxis("Vertical");
+
+        if (axisV > 0)
+        {
+            animator.SetBool("Walking", true);
+            transform.Translate(Vector3.forward * walkSpeed * axisV * Time.deltaTime);
+        }
+        if (axisV == 0)
+        {
+            animator.SetBool("Walking", false);
+        }
+    }
+}
